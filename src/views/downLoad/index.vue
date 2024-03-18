afb91bf145a29c2fc067e67a8cd8010ece0fb8f4
xumenghao1225 <xumenghao1225@163.com>
2024-03-18 17:34:47 +0800

    wip: 🧠 下载组件kaifa

---
+++ src/views/downLoad/index.vue
@@ -0,0 +1,19 @@
+<style lang="scss" scoped></style>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  name: "DownLoad",
+})
+export default class DownLoad extends Vue {
+  fileList = [{}];
+}
+</script>
+
+<template>
+  <div class="download-container-wrapper">
+    <div>
+      <el-table :data="fileList" style="width: 100%"></el-table>
+    </div>
+  </div>
+</template>
